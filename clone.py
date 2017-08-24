5d060eff6e0c4e77b19b500ec48e84802b68628b
Shohei Fujii <fujii.shohei@gmail.com>
2017-08-24 09:51:25 +0900

    crop the bottom of the image

--- clone.py
+++ clone.py
@@ -6,22 +6,33 @@ import numpy as np
 import os
 
 datadir = os.path.expanduser('~/sandbox/udacity')
-lines = []
+images = []
+measurements = []
+
 with open(os.path.join(datadir, 'driving_log.csv')) as f:
     reader = csv.reader(f)
     for line in reader:
-        lines.append(line)
+        def getimage(imgpathcolumn, measurementcolumn):
+            source_path = line[imgpathcolumn]
+            measurement = float(line[measurementcolumn])
+            imgpath = os.path.join(datadir, 'IMG', os.path.basename(source_path))
+            image = cv2.imread(imgpath)
+            return image, measurement
+        # center
+        correction = 0.2
+        image, measurement = getimage(0, 3)
+        images.append(image)
+        measurements.append(measurement)
 
-images = []
-measurements = []
-for line in lines:
-    source_path = line[0]
-    measurement = float(line[3])
-
-    imgpath = os.path.join(datadir, 'IMG', os.path.basename(source_path))
-    image = cv2.imread(imgpath)
-    images.append(image)
-    measurements.append(measurement)
+        # left
+        image, measurement = getimage(1, 3)
+        images.append(image)
+        measurements.append(measurement + correction)
+        
+        # right
+        image, measurement = getimage(2, 3)
+        images.append(image)
+        measurements.append(measurement - correction)
 
 # X_train = np.array(images)
 # y_train = np.array(measurements)
@@ -37,10 +48,11 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPooling2D, Cropping2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x/255.0 - 0.5), input_shape = (160, 320, 3)))
+model.add(Cropping2D(cropping=((70, 25), (0,0))))
 model.add(Conv2D(6, 5, 5, activation='relu'))
 model.add(MaxPooling2D())
 model.add(Conv2D(6, 5, 5, activation='relu'))
